21440d832de9b9658b18ab70fbfd829f2ff33bdb
codakairos <112564682+codakairos@users.noreply.github.com>
2026-05-20 15:17:09 +0200

    fix: Vote Race Condition in Quote Views (#121)
    Closes #93
    
    Co-authored-by: cfazilleau <git@cfaz.dev>

--- cogs/quotes/quotes.py
+++ cogs/quotes/quotes.py
@@ -123,18 +123,26 @@ class QuoteView(discord.ui.View):
 
     async def _vote(self, interaction: discord.Interaction, field: str, opposite_field: str):
         user_id = str(interaction.user.id)
-        if user_id in self.current_quote.get(field, []):
+        collection: AsyncCollection = await self.quotes_cog.bot.mongo.get_collection(self.guild_id, 'quotes')
+
+        # Atomic check-and-update
+        result = await collection.update_one(
+            {'_id': self.current_quote['_id'], field: {'$ne': user_id}},
+            {
+                '$addToSet': {field: user_id},
+                '$pull': {opposite_field: user_id}
+            }
+        )
+
+        if result.matched_count == 0:
             await interaction.response.send_message('You already voted on this quote.', ephemeral=True)
             return
 
-        collection: AsyncCollection = await self.quotes_cog.bot.mongo.get_collection(self.guild_id, 'quotes')
-        update: dict = {'$push': {field: user_id}}
+        # Update in-memory state
         if user_id in self.current_quote.get(opposite_field, []):
-            update['$pull'] = {opposite_field: user_id}
             self.current_quote[opposite_field].remove(user_id)
-
-        await collection.update_one({'_id': self.current_quote['_id']}, update)
         self.current_quote.setdefault(field, []).append(user_id)
+
         self._remove_reroll()
         self._update_vote_buttons()
 
